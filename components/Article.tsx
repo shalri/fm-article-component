cb22fa36fc1f0a8582ed480367662b6069deee76
shalri <fsaonlinework777@gmail.com>
2024-06-01 20:01:34 +0800

    Desktop layout

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { Articles, shareLinks } from "../lib/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export default function Article({ articleTitle }: { articleTitle: string }) {
@@ -10,10 +10,26 @@ export default function Article({ articleTitle }: { articleTitle: string }) {
   );
   const [share, setShare] = useState(false);
   const [animate, setAnimate] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const updateScreenWidth = () => {
+      setIsWideScreen(window.innerWidth > 640);
+    };
+
+    updateScreenWidth();
+    window.addEventListener("resize", updateScreenWidth);
+
+    return () => window.removeEventListener("resize", updateScreenWidth);
+  }, []);
 
   const handleClick = () => {
-    setAnimate(!animate);
-    setShare(!share);
+    if (isWideScreen) {
+      console.log("hello");
+    } else {
+      setAnimate(!animate);
+      setShare(!share);
+    }
   };
 
   if (!article) {
@@ -21,33 +37,35 @@ export default function Article({ articleTitle }: { articleTitle: string }) {
   }
 
   return (
-    <article className="gird max-w-[400px] grid-rows-1 overflow-hidden rounded-xl bg-white tracking-[0.01rem]">
-      <div className="relative h-[200px] w-full overflow-hidden">
+    <article className="grid max-w-[400px] grid-rows-1 overflow-hidden rounded-xl bg-white tracking-[0.01rem] sm:max-w-[732px] sm:grid-cols-[40%_60%]">
+      <div className="relative h-[200px] w-full overflow-hidden sm:h-auto sm:w-full">
         <Image
           src={article.img}
           alt="Article Image"
           width={640}
           height={640}
           priority
-          className="-mt-4 object-cover object-top"
+          className="-mt-4 h-full object-cover object-top sm:-mr-4 sm:mt-0 sm:w-full sm:object-left"
         />
       </div>
       <section className="flex flex-col items-center justify-center pt-8">
-        <h1 className="mb-3 mt-1 px-8 text-[1.025rem] font-bold leading-[1.50rem] tracking-[0.0125rem] text-ac-very-dark-grayish-blue">
+        <h1 className="mb-3 mt-1 px-8 text-[1.025rem] font-bold leading-[1.50rem] tracking-[0.0125rem] text-ac-very-dark-grayish-blue sm:px-10 sm:text-xl sm:leading-[1.75rem]">
           {article.heading}
         </h1>
-        <p className="px-8 text-ac-desaturated-dark-blue">{article.intro}</p>
+        <p className="px-8 text-ac-desaturated-dark-blue sm:px-10">
+          {article.intro}
+        </p>
         <footer
           className={cn(
-            "mt-8 flex w-full items-center justify-between",
+            "mt-8 flex w-full items-center justify-between sm:mb-2 sm:mt-4",
             share && animate
               ? "fade-in-bounce bg-ac-very-dark-grayish-blue"
               : "bg-white pb-6",
           )}
         >
           {share ? (
-            <div className="-mr-16 flex h-16 flex-grow items-center justify-start rounded-b-xl px-8 text-ac-light-grayish-blue">
-              <h3 className="pr-4 text-body uppercase tracking-[0.4rem] opacity-40">
+            <div className="sm:px-10text-ac-light-grayish-blue -mr-16 flex h-16 flex-grow items-center justify-start rounded-b-xl px-8">
+              <h3 className="pr-4 text-body uppercase tracking-[0.4rem] text-ac-grayish-blue">
                 share
               </h3>
               <ul className="flex items-center gap-4">
@@ -62,7 +80,7 @@ export default function Article({ articleTitle }: { articleTitle: string }) {
             </div>
           ) : (
             <div className="fade-in-bounce">
-              <div className=" flex items-center justify-center gap-4 px-8">
+              <div className=" flex items-center justify-center gap-4 px-8 sm:px-10">
                 <Image
                   src={article.author.img}
                   width={40}
