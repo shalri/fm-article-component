9ad04987060a27e02452dbdcc3eb19a9ca869f8e
shalri <fsaonlinework777@gmail.com>
2024-06-01 17:45:33 +0800

    Mobile Share UI

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,24 +1,28 @@
 "use client";
 import Image from "next/image";
 import { Articles, shareLinks } from "../lib/data";
-import shareIcon from "../public/images/icon-share.svg";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Article({ articleTitle }: { articleTitle: string }) {
   const article = Articles.find(
     (article) => article.title.toLowerCase() === articleTitle.toLowerCase(),
   );
   const [share, setShare] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    setAnimate(!animate);
+    setShare(!share);
+  };
 
   if (!article) {
     return <div>No articles to show</div>;
   }
 
   return (
-    <article className="flex flex-col items-center justify-center rounded-lg bg-white tracking-[0.01rem]">
-      <div className="relative h-[200px] w-full overflow-hidden rounded-t-lg">
+    <article className="gird max-w-[400px] grid-rows-1 overflow-hidden rounded-xl bg-white tracking-[0.01rem]">
+      <div className="relative h-[200px] w-full overflow-hidden">
         <Image
           src={article.img}
           alt="Article Image"
@@ -28,35 +32,73 @@ export default function Article({ articleTitle }: { articleTitle: string }) {
           className="-mt-4 object-cover object-top"
         />
       </div>
-      <div className="px-8 pb-6 pt-8">
-        <h1 className="mb-3 mt-1 text-[1.025rem] font-bold leading-normal tracking-[0.0125rem] text-ac-very-dark-grayish-blue">
+      <section className="flex flex-col items-center justify-center pt-8">
+        <h1 className="mb-3 mt-1 px-8 text-[1.025rem] font-bold leading-[1.50rem] tracking-[0.0125rem] text-ac-very-dark-grayish-blue">
           {article.heading}
         </h1>
-        <p className="text-ac-desaturated-dark-blue">{article.intro}</p>
-        <div className="mt-8 flex w-full items-center justify-between">
-          <div className="flex gap-4">
-            <Image
-              src={article.author.img}
-              width={40}
-              height={40}
-              alt={`${article.author.name} avatar`}
-              className="rounded-full"
-            />
-            <div>
-              <h2 className="font-bold text-ac-very-dark-grayish-blue">
-                {article.author.name}
-              </h2>
-              <p className="text-ac-grayish-blue">{article.date}</p>
+        <p className="px-8 text-ac-desaturated-dark-blue">{article.intro}</p>
+        <footer
+          className={cn(
+            "mt-8 flex w-full items-center justify-between",
+            share && animate
+              ? "fade-in-bounce bg-ac-very-dark-grayish-blue"
+              : "bg-white pb-6",
+          )}
+        >
+          {share ? (
+            <div className="-mr-16 flex h-16 flex-grow items-center justify-start rounded-b-xl px-8 text-ac-light-grayish-blue">
+              <h3 className="pr-4 text-body uppercase tracking-[0.4rem] opacity-40">
+                share
+              </h3>
+              <ul className="flex items-center gap-4">
+                {shareLinks.map((link) => (
+                  <li key={link.site}>
+                    <a href={link.url}>
+                      <Image src={link.icon} alt={link.site} />
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
-          </div>
+          ) : (
+            <div className="fade-in-bounce">
+              <div className=" flex items-center justify-center gap-4 px-8">
+                <Image
+                  src={article.author.img}
+                  width={40}
+                  height={40}
+                  alt={`${article.author.name} avatar`}
+                  className="rounded-full"
+                />
+                <div>
+                  <h2 className="font-bold text-ac-very-dark-grayish-blue">
+                    {article.author.name}
+                  </h2>
+                  <p className="text-ac-grayish-blue">{article.date}</p>
+                </div>
+              </div>
+            </div>
+          )}
           <button
             onClick={handleClick}
-            className="flex h-8 w-8 items-center justify-center rounded-full bg-ac-light-grayish-blue"
+            className={cn(
+              "mr-8 flex h-8 w-8 items-center justify-center rounded-full",
+              share
+                ? "bg-ac-desaturated-dark-blue"
+                : "bg-ac-light-grayish-blue",
+            )}
           >
-            <Image src={shareIcon} alt="share icon" className="object-center" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="13"
+              className={cn(share ? "fill-white" : "fill-[#6E8098]")}
+            >
+              <path d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z" />
+            </svg>
           </button>
-        </div>
-      </div>
+        </footer>
+      </section>
     </article>
   );
 }
